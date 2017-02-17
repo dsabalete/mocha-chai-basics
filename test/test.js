715598b9b270a6b331166018101e1fbcbc213e7e
David Sabalete <dsabalete@gmail.com>
2017-02-17 17:54:06 +0100

    My first test with Chai Mocha

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('A basic test', function() {
+    it('should pass when everyting is okay', function() {
+        expect(true).to.be.true;
+    });
+});
